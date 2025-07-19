0edbfd483cb52219aaedda0d22cf5f047e7f89ba
LuxuryHub <fozyann@gmail.com>
2025-07-20 00:10:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from bypass import cookie_fresher  # из твоего кода
 import logging
 
-TOKEN = "ТВОЙ_ТОКЕН_БОТА"
+API_TOKEN = "8145614744:AAFSqIjLnxlvEPEhe9e1U_Vbcw6SOBdiHH0"
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
